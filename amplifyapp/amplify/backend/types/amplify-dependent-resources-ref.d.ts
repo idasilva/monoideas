d4b497fe26a006fa73201e42abbb0206abd3e823
Z204544 <z204544@corp.clarobr>
2023-03-11 19:16:44 -0300

    added auth

---
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsserverlessamplifydb65c241": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
