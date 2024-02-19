6bfe21c61f3c531fb0fbf0674942813d86e84a1e
Isaias da Silva <isaias.silva@itera.com.br>
2024-02-19 17:29:01 -0300

    add pem file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
  #!/bin/bash
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
+#mkdir -p ~/.ssh
+#chmod 700 ~/.ssh
 
-ssh-keyscan -H -t rsa 172.31.111.208 >> ~/.ssh/known_hosts
+#ssh-keyscan -H -t rsa 172.31.111.208 >> ~/.ssh/known_hosts
 
 script="aws ec2-instance-connect ssh  \
             --instance-id i-0cf5ec99fdb293e01 \
             --connection-type eice  \
             --os-user ubuntu  \
             --region us-east-1  \
+            --private-key-file ./endpoint.pem \
             --eice-options maxTunnelDuration=900"
 
 bash -c "$script <<EOF
