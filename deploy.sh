7f7c948d673ef18bb519709fa5be4f14e459193b
Isaias da Silva <isaias.silva@itera.com.br>
2024-02-19 20:26:02 -0300

    remove to test

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,18 @@
  #!/bin/bash
 
 #script="aws ec2-instance-connect ssh  \
-  #          --instance-id i-0560fb61b4ed18c3a \
- #           --instance-ip 172.31.111.208 \
+ #           --instance-id i-0560fb61b4ed18c3a \
+  #          --instance-ip 172.31.111.208 \
   #          --connection-type eice  \
-  #          --os-user ubuntu  \
-   #         --region us-east-1  \
-  #          --private-key-file ./endpoint.pem   \
-   #         --eice-options maxTunnelDuration=900 \
-   #         --no-cli-auto-prompt"
+ #           --os-user ubuntu  \
+   #        --region us-east-1  \
+   #         --private-key-file ./endpoint.pem   \
+  #          --eice-options maxTunnelDuration=900 \
+   #        --no-cli-auto-prompt"
 
 
 echo "AQUUUUI"
+
 #bash -c "$script <<EOF
 #  ls -a
 #  mkdir enodes
