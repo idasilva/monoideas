f9a5e94654ee249e9127e425728270d985c37d1e
Isaias da Silva <isaias.silva@itera.com.br>
2024-02-19 18:12:44 -0300

    tests

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,13 @@
 
 script="aws ec2-instance-connect ssh  \
             --instance-id i-0560fb61b4ed18c3a \
+            --instance-ip 172.31.111.208 \
             --connection-type eice  \
             --os-user ubuntu  \
             --region us-east-1  \
-            --private-key-file ./endpoint.pem \
-            --eice-options maxTunnelDuration=900"
+            --private-key-file ./endpoint.pem   \
+            --eice-options maxTunnelDuration=900 \
+            --no-cli-auto-prompt"
 
 bash -c "$script <<EOF
   ls -a
