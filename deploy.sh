7b2dcc30390eaa05b867c66b168f04932aa4bade
Isaias da Silva <isaias.silva@itera.com.br>
2024-02-19 20:35:05 -0300

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,20 @@
 
 echo "AQUUUUI"
 
+aws ec2-instance-connect ssh  \
+  --instance-id i-0560fb61b4ed18c3a \
+  --instance-ip 172.31.111.208 \
+  --connection-type eice  \
+  --os-user ubuntu  \
+  --region us-east-1  \
+  --private-key-file ./endpoint.pem   \
+  --eice-options maxTunnelDuration=900<<EOT
+    echo "Executing command block 1"
+    ls -a
+EOT
+
+echo "AQUUUU12"
+
 #bash -c "$script <<EOF
 #  ls -a
 #  mkdir enodes
