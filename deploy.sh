58d62ca4e47d00cc7e986e902c6f59dc8a4c8097
Isaias da Silva <isaias.silva@itera.com.br>
2024-02-19 22:58:35 -0300

    via pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,17 @@
  #!/bin/bash
 
-#script="aws ec2-instance-connect ssh  \
- #           --instance-id i-0560fb61b4ed18c3a \
-  #          --instance-ip 172.31.111.208 \
-  #          --connection-type eice  \
- #           --os-user ubuntu  \
-   #        --region us-east-1  \
-   #         --private-key-file ./endpoint.pem   \
-  #          --eice-options maxTunnelDuration=900 \
-   #        --no-cli-auto-prompt"
-
-
 echo "AQUUUUI"
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-eval `ssh-agent -s`
-echo "$(cat ./endpoint.pem)" | ssh-add -
-echo "" > ~/.ssh/known_hosts 
-ssh-keyscan -H "172.31.124.225" >> ~/.ssh/known_hosts
+cat > "$HOME/.ssh/config" <<EOL
+Host 172.31.124*
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOL
 
-ssh-keygen -R
+chmod 600 "$HOME/.ssh/config"
 
 aws ec2-instance-connect ssh  \
   --instance-id i-0973c4c7d024bd386 \
@@ -33,12 +23,7 @@ aws ec2-instance-connect ssh  \
   --eice-options maxTunnelDuration=900<<EOT
     echo "Executing command block 1"
     ls -a
+    mkdir viapipeline
 EOT
 
 echo "AQUUUU12"
-
-#bash -c "$script <<EOF
-#  ls -a
-#  mkdir enodes
-#  ls -a
-#EOF"
