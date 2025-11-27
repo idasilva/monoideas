7bc68667b9472769a5e7d3c917b36e484ca9be30
isc <isaias.carvalho986@gmail.com>
2025-11-27 09:48:37 -0300

    improve k8s version

--- iac/terraform/eks/main.tf
+++ iac/terraform/eks/main.tf
@@ -8,7 +8,7 @@ module "eks_cluster" {
   version = "~> 20.24"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.30"
+  cluster_version = "1.32"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
